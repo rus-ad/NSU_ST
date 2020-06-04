9341b4582305dba17b4aebd6321d283d142f8d13
Rusnak Alexandr <rus91ad@mail.ru>
2020-06-04 13:06:28 +0700

    done task 4

---
+++ task_4/OrientDB/install_orientdb.sh
@@ -0,0 +1,2 @@
+docker run --name orientdb -p 2424:2424 -p 2480:2480 -e ORIENTDB_ROOT_PASSWORD=root 
+
---
+++ task_4/redis/run_all.sh
@@ -0,0 +1,8 @@
+redis-cli < 1.redisCmds
+redis-cli < 2.redisCmds
+redis-cli < 3.redisCmds
+redis-cli < 4.redisCmds
+redis-cli < 5.redisCmds
+redis-cli < 6.redisCmds
+redis-cli < 7.redisCmds
+
